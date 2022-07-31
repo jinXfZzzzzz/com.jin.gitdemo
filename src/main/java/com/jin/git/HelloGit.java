080df089dfe0867f47719ea61f790473e76d7595
jin <jin168313@163.com>
2022-07-31 14:44:40 +0800

    hot-fix commit1

--- src/main/java/com/jin/git/HelloGit.java
+++ src/main/java/com/jin/git/HelloGit.java
@@ -11,6 +11,6 @@ public class HelloGit {
         System.out.println("hello,git");
         System.out.println("hello,git2");
         System.out.println("hello,git3");
-        System.out.println("hello,hot-fix");
+        System.out.println("hello,hot-fix2");
     }
 }
