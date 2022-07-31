afd4f95eed39daf6fabc66ad9e12f3ef5d7674d7
jin <jin168313@163.com>
2022-07-31 13:49:45 +0800

    first commit

---
+++ src/main/java/com/jin/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.jin.git;
+
+/**
+ * @Description:
+ * @PackageName: com.jin.git
+ * @author: LiangJH
+ * @create 2022-07-31 13:46
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
